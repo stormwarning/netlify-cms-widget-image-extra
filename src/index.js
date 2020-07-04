4e02f6150baa83ef77aec59d3a5deaa673255fc6
Jeff <rustyangel@gmail.com>
2020-07-03 20:47:50 -0600

    New: Add starter widget files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import withFileControl from './withFileControl'
-import previewComponent from './FilePreview'
-import schema from './schema'
+import Control from './control'
+import Preview from './preview'
 
-const controlComponent = withFileControl()
-const Widget = (opts = {}) => ({
-    name: 'file',
-    controlComponent,
-    previewComponent,
-    schema,
-    ...opts,
-})
-
-export const NetlifyCmsWidgetFile = {
-    Widget,
-    controlComponent,
-    previewComponent,
-    withFileControl,
+if (typeof window !== 'undefined') {
+  window.Control = Control
+  window.Preview = Preview
 }
-export default NetlifyCmsWidgetFile
+
+export { Control, Preview }
+// import withFileControl from './withFileControl'
+// import previewComponent from './FilePreview'
+// import schema from './schema'
+
+// const controlComponent = withFileControl()
+// const Widget = (opts = {}) => ({
+//     name: 'file',
+//     controlComponent,
+//     previewComponent,
+//     schema,
+//     ...opts,
+// })
+
+// export const NetlifyCmsWidgetFile = {
+//     Widget,
+//     controlComponent,
+//     previewComponent,
+//     withFileControl,
+// }
+// export default NetlifyCmsWidgetFile
